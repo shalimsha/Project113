a7a0a1a4f67afa0d218d9e84f548f789105c84b5
shalimsha <mohamedshalim49@gmail.com>
2022-02-08 07:32:50 +0530

    shalim task done

--- src/org/srs/Employee.java
+++ src/org/srs/Employee.java
@@ -1,5 +1,13 @@
 package org.srs;
 
 public class Employee {
-
+             
+	public void mul () {
+		System.out.println("multiplication");
+		
+	}
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.mul();
+	}
 }
